d8eff998a6320dba7a083b98df488e4e76edf7ae
Joshua Berrios <64435634+JoshuaB94@users.noreply.github.com>
2022-05-14 12:27:50 -0500

    Created CreatorGrid Component to house creators that will be available via the backend directory. Just need to replace certain elements with social buttons.

---
+++ tutorial-flare/components/CreatorGrid/CreatorGrid.tsx
@@ -0,0 +1,92 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { MailIcon, PhoneIcon } from '@heroicons/react/solid'
+
+const people = [
+  {
+    name: 'Jane Cooper',
+    title: 'Video Creator',
+    location: 'Miami, FL',
+    email: 'janecooper@example.com',
+    telephone: '+1-202-555-0170',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+  },
+  {
+    name: 'Joe Dane',
+    title: 'Video Creator',
+    location: 'Omaha, NE',
+    email: 'janecooper@example.com',
+    telephone: '+1-202-555-0170',
+    imageUrl:
+      'https://images.unsplash.com/photo-1495184134775-1b80cca0fa62?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+  },
+  {
+    name: 'Duke Hinkle',
+    title: 'Video Creator',
+    location: 'New York City, NY',
+    email: 'janecooper@example.com',
+    telephone: '+1-202-555-0170',
+    imageUrl:
+      'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+  },
+  {
+    name: 'Ah Lam',
+    title: 'Video Creator',
+    location: 'Los Angeles, CA',
+    email: 'janecooper@example.com',
+    telephone: '+1-202-555-0170',
+    imageUrl:
+      'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+  },
+  // More people...
+]
+
+export default function CreatorGrid() {
+  return (
+    <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {people.map((person) => (
+        <li
+          key={person.email}
+          className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200"
+        >
+          <div className="flex-1 flex flex-col p-8">
+            <img className="w-32 h-32 flex-shrink-0 mx-auto rounded-full" src={person.imageUrl} alt="" />
+            <h3 className="mt-6 text-gray-900 text-sm font-medium">{person.name}</h3>
+            <dl className="mt-1 flex-grow flex flex-col justify-between">
+              <dt className="sr-only">Title</dt>
+              <dd className="text-gray-500 text-sm">{person.title}</dd>
+              <dt className="sr-only">Role</dt>
+              <dd className="mt-3">
+                <span className="px-2 py-1 text-green-800 text-xs font-medium bg-green-100 rounded-full">
+                  {person.location}
+                </span>
+              </dd>
+            </dl>
+          </div>
+          <div>
+            <div className="-mt-px flex divide-x divide-gray-200">
+              <div className="w-0 flex-1 flex">
+                <a
+                  href={`mailto:${person.email}`}
+                  className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
+                >
+                  <MailIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
+                  <span className="ml-3">Email</span>
+                </a>
+              </div>
+              <div className="-ml-px w-0 flex-1 flex">
+                <a
+                  href={`tel:${person.telephone}`}
+                  className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
+                >
+                  <PhoneIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
+                  <span className="ml-3">Call</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
+}
--- tutorial-flare/pages/index.tsx
+++ tutorial-flare/pages/index.tsx
@@ -1,11 +1,17 @@
 import type { NextPage } from 'next'
 import HomeHero from '../components/HeroSection/MainHero'
+import CreatorGrid from '../components/CreatorGrid/CreatorGrid'
 
 
 const Home: NextPage = () => {
   return (
     < >
       <HomeHero />
+      <div className="container mx-auto max-w-full px-8 py-20 bg-orange-600">
+        <div className="mx-auto max-w-7xl">
+          <CreatorGrid />
+        </div>
+      </div>
     </>
   )
 }
