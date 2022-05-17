97a6341baa390beb9c5202530e73696830adb88c
Joshua Berrios <64435634+JoshuaB94@users.noreply.github.com>
2022-05-16 23:09:01 -0500

    Coded the Supabase Logic for a Magic Link Signup and Sign In ability + Need to code logic for SSO

--- tutorial-flare/components/Login/LoginPage.tsx
+++ tutorial-flare/components/Login/LoginPage.tsx
@@ -1,20 +1,20 @@
-/*
-  This example requires Tailwind CSS v2.0+ 
-  
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
+import { useState } from 'react';
+import { supabase } from '../../utils/supabaseClient';
+
 export default function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  async function logIn() {
+    if (!email) return
+    const {error} = await supabase.auth.signIn({email})
+    if (error) {
+      console.log({error})
+    } else {
+      setSubmitted(true)
+    }
+  }
+
     return (
       <>
         <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -47,11 +47,12 @@ export default function LoginPage() {
                       autoComplete="email"
                       required
                       className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-600 sm:text-sm"
+                      onChange={e => setEmail(e.target.value)}
                     />
                   </div>
                 </div>
   
-                <div>
+                {/* <div>
                   <label htmlFor="password" className="block text-sm font-medium text-gray-700">
                     Password
                   </label>
@@ -85,14 +86,15 @@ export default function LoginPage() {
                       Forgot your password?
                     </a>
                   </div>
-                </div>
+                </div> */}
   
                 <div>
                   <button
                     type="submit"
                     className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange-600 hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    onClick={() => logIn()}
                   >
-                    Sign in
+                    Send Magic Link
                   </button>
                 </div>
               </form>
--- tutorial-flare/pages/_app.tsx
+++ tutorial-flare/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import MainFooter from '../components/Footer/MainFooter'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
+      {/* <MainFooter /> */}
     </ChakraProvider>
   )
 }
